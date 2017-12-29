b95819fcf3c3d1f1930e0b0352151ede01918da4
Pyxis <pyxis@uzuki.live>
2017-12-29 11:06:29 +0900

    documenting

--- library/src/main/java/pyxis/uzuki/live/actionsheet/config/ActionSheetConfig.kt
+++ library/src/main/java/pyxis/uzuki/live/actionsheet/config/ActionSheetConfig.kt
@@ -24,26 +24,90 @@ data class ActionSheetConfig(val items: List<ActionButton>, val cancelButton: Ac
         private var onActionButtonClickListener: OnActionButtonClickListener? = null
         private var cancelable: Boolean = false
 
+        /**
+         * add item with Strings, color will set by default (Color.BLACK)
+         *
+         * @param[items] vararg of title of button
+         */
         fun addItem(vararg items: String) = apply { this.items.addAll(items.map { ActionButton(it) }) }
 
+        /**
+         * add item with Strings, ColorInt.
+         *
+         * @param[title] title of button
+         * @param[color] ColorInt
+         */
         fun addItem(title: String, @ColorInt color: Int) = apply { this.items.add(ActionButton(title, color)) }
 
+        /**
+         * add items with ActionButton
+         *
+         * @param[items] vararg of [ActionButton] object
+         */
         fun addItem(vararg items: ActionButton) = apply { this.items.addAll(items) }
 
+        /**
+         * set items with ArrayList<ActionButton>
+         *
+         * @param[items] ArrayList of ActionButton
+         */
         fun setItems(items: ArrayList<ActionButton>) = apply { this.items.addAll(items) }
 
+        /**
+         * set items with List<ActionButton>
+         *
+         * @param[items] List of ActionButton
+         */
+        fun setItems(items: List<ActionButton>) = apply { this.items.addAll(items) }
+
+        /**
+         * set cancel button with String, color will set by default (Color.BLACK)
+         *
+         * @param[title] vararg of title of button
+         */
         fun setCancelButton(title: String) = apply { this.cancelButton = ActionButton(title) }
 
+        /**
+         * set cancel button with String, color
+         *
+         * @param[title] title of button
+         * @param[color] ColorInt
+         */
         fun setCancelButton(title: String, @ColorInt color: Int) = apply { this.cancelButton = ActionButton(title, color) }
 
+        /**
+         * set cancel button with ActionButton
+         *
+         * @param[cancelButton] [ActionButton] object
+         */
         fun setCancelButton(cancelButton: ActionButton) = apply { this.cancelButton = cancelButton }
 
+        /**
+         * set listener for dismissing
+         *
+         * @param[listener] [OnDismissListener] object
+         */
         fun setOnDismissListener(listener: OnDismissListener) = apply { this.onDismissListener = listener }
 
+        /**
+         * set listener for click button
+         *
+         * @param[listener] [OnActionButtonClickListener] object
+         */
         fun setOnActionButtonClickListener(listener: OnActionButtonClickListener) = apply { this.onActionButtonClickListener = listener }
 
+        /**
+         * set flag of dismiss when touch outside of ActionSheet
+         *
+         * @param[cancelable] true if dismiss when touch outside of ActionSheet, otherwise false
+         */
         fun setCancelableOnTouchOutside(cancelable: Boolean) = apply { this.cancelable = cancelable }
 
+        /**
+         * build ActionSheetConfig.
+         *
+         * to show ActionSheet, you may call ActionSheet.show(this, getSupportFragmentManager(), config);
+         */
         fun build() = ActionSheetConfig(items, cancelButton, onDismissListener, onActionButtonClickListener, cancelable)
     }
 }
